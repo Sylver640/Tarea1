06fbbf1e641e258d7e6456a02d9e026bbb4166ea
Vicente Mercado <vicente.mercado.m@mail.pucv.cl>
2022-03-25 13:48:54 -0300

    Creación parcial de structs ListaCanciones y tipoCancion

--- main.c
+++ main.c
@@ -5,6 +5,23 @@
 #include <stdbool.h>
 #include "list.h"
 
+typedef struct{
+    int cantidad;
+    // canciones(Lista);
+    char nombre[30];
+
+} ListaCanciones;
+
+typedef struct{
+    char nombreC[30];
+    int year;
+    char artista[15];
+    // generos(Lista);
+    // listaC(ListaCanciones);
+
+
+} tipoCancion;
+
 int main()
 {
     printf("Hola Mundo\n");
