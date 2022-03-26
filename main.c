160a9ac467c8e2e4fd63dfa9b9af06ece6fc3e9f
Your Name <you@example.com>
2022-03-26 20:58:23 -0300

    Se agrega el menú

--- main.c
+++ main.c
@@ -22,5 +22,40 @@ typedef struct{
 void main()
 {
     FILE *archivoCanciones = fopen("Canciones.csv", "rw");
+    int option;
+
+    while (option != 8)
+    {
+        printf("1. Importar canciones desde el archivo\n");
+        printf("2. Exportar canciones (CSV)\n");
+        printf("3. Agregar cancion\n");
+        printf("4. Buscar cancion por nombre\n");
+        printf("5. Buscar cancion por artista\n");
+        printf("6. Buscar cancion por genero\n");
+        printf("7. Eliminar cancion\n");
+        printf("8. Salir\n");
+
+        printf("Indique la opcion: ");
+        scanf("%i", &option);
+
+        switch(option)
+        {
+            case 1: printf("FUNCION NO IMPLEMENTADA\n");
+                    break;
+            case 2: printf("FUNCION NO IMPLEMENTADA\n");
+                    break;
+            case 3: printf("FUNCION NO IMPLEMENTADA\n");
+                    break;
+            case 4: printf("FUNCION NO IMPLEMENTADA\n");
+                    break;
+            case 5: printf("FUNCION NO IMPLEMENTADA\n");
+                    break;
+            case 6: printf("FUNCION NO IMPLEMENTADA\n");
+                    break;
+            case 7: printf("FUNCION NO IMPLEMENTADA\n");
+                    break;
+            case 8: break;
+        }
+    }
     return;
 }
